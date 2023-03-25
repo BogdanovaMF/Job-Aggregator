0b6bc10b52cb268adbb765a98e5699ab26972e6d
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-03-25 18:01:39 +0300

    fix_bages

--- db/init_db.py
+++ db/init_db.py
@@ -1,21 +1,22 @@
 from utilities import *
 
-query = """ 
-    CREATE TABLE IF NOT EXISTS vacancies (
-        id SERIAL PRIMARY KEY,
-        pub_date DATE,
-        update_ts TIMESTAMP WITH TIME ZONE,
-        csv_date TIMESTAMP WITH TIME ZONE,
-        source_type CHARACTER VARYING (20),
-        vacancy CHARACTER VARYING (100),
-        experience CHARACTER VARYING (50),
-        company CHARACTER VARYING (128),
-        link CHARACTER VARYING (100),
-        salary CHARACTER VARYING (50),
-        skills CHARACTER VARYING (300),
-        description VARCHAR
-    ); 
-"""
+if __name__ == '__main__':
+    query = """ 
+        CREATE TABLE IF NOT EXISTS vacancies (
+            id SERIAL PRIMARY KEY,
+            pub_date DATE,
+            update_ts TIMESTAMP WITH TIME ZONE,
+            csv_date TIMESTAMP WITH TIME ZONE,
+            source_type CHARACTER VARYING (20),
+            vacancy CHARACTER VARYING (100),
+            experience CHARACTER VARYING (50),
+            company CHARACTER VARYING (128),
+            link CHARACTER VARYING (100),
+            salary CHARACTER VARYING (50),
+            skills CHARACTER VARYING (300),
+            description VARCHAR
+        ); 
+    """
 
-cursor.execute(query)
-conn.commit()
+    cursor.execute(query)
+    conn.commit()
--- db/utilities.py
+++ db/utilities.py
@@ -1,6 +1,10 @@
 import os
 import psycopg2
 
-conn = psycopg2.connect(dbname="postgres", user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'],
-                        host=os.environ['DB_HOST'], port=os.environ['DB_PORT'])
-cursor = conn.cursor()
+def get_pg_connection():
+    conn = psycopg2.connect(dbname="postgres",
+                            user=os.environ['DB_USER'],
+                            password=os.environ['DB_PASSWORD'],
+                            host=os.environ['DB_HOST'],
+                            port=os.environ['DB_PORT'])
+    return conn
