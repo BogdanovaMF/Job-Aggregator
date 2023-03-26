86084d2a711ab729104f9371cf7f9082fe9f6016
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-03-26 08:07:10 +0300

    remarks corrected

--- db/init_db.py
+++ db/init_db.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
             pub_date DATE,
             update_ts TIMESTAMP WITH TIME ZONE,
             csv_date TIMESTAMP WITH TIME ZONE,
-            source_type CHARACTER VARYING (20),
+            source_upload_ts CHARACTER VARYING (20),
             vacancy CHARACTER VARYING (150),
             experience CHARACTER VARYING (50),
             company CHARACTER VARYING (128),
@@ -15,7 +15,29 @@ if __name__ == '__main__':
             salary CHARACTER VARYING (50),
             skills CHARACTER VARYING (300),
             description VARCHAR
-        ); 
+        );
+        COMMENT ON COLUMN vacancies.pub_date 
+            IS 'Дата публикации вакансии';
+        COMMENT ON COLUMN vacancies.update_ts 
+            IS 'Дата вставки данных в таблицу vacancies';
+        COMMENT ON COLUMN vacancies.csv_date 
+            IS 'Дата создания csv-файла, из которого выгружается информация';
+        COMMENT ON COLUMN vacancies.source_upload_ts 
+            IS 'Источник полученной инфомарции о вакансиях ';
+        COMMENT ON COLUMN vacancies.vacancy 
+            IS 'Наименование вакансии';
+        COMMENT ON COLUMN vacancies.experience 
+            IS 'Требуемый опыт';
+        COMMENT ON COLUMN vacancies.company 
+            IS 'Название компании';
+        COMMENT ON COLUMN vacancies.link 
+            IS 'Ссылка на вакансию';
+        COMMENT ON COLUMN vacancies.salary 
+            IS 'Заработная плата';
+        COMMENT ON COLUMN vacancies.skills 
+            IS 'Ключевые навыки, требуемые в вакансии'; 
+        COMMENT ON COLUMN vacancies.description 
+            IS 'Описание вакансии';
     """
 
     conn = get_pg_connection()
