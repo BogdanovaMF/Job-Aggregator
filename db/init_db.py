b9ab9713b91f22a7da4e98508cfc82aa76965b9c
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-03-26 09:08:57 +0300

    JA 5 create table in database (#9)
    * add script with additional fields
    
    * using the parsel library, xpath search, adding new fields
    
    * bugs fixed, script run via argparse, script divided into functions
    
    * bugs fixed, script rewritten to a class, type annotations and method documentation addedbugs fixed, script run via argparse, script divided into functions
    
    * bugs fixed, functions documented according to pep257
    
    * Update hh_parser.py
    
    Co-authored-by: Rostislav <36924856+g-rostislav@users.noreply.github.com>
    
    * bugs fixed
    
    * database creation
    
    * create table in database
    
    * correcting remarks when creating a table
    
    * fix_bages
    
    * some fixes
    
    * 1231232
    
    * ferf
    
    * fixing bugs, adding function for connection
    
    * remarks corrected
    
    * correction of remarks
    
    * correction of remarks
    
    ---------
    
    Co-authored-by: Rostislav <36924856+g-rostislav@users.noreply.github.com>

---
+++ db/init_db.py
@@ -0,0 +1,46 @@
+from utilities import get_pg_connection
+
+if __name__ == '__main__':
+    query = """ 
+        CREATE TABLE IF NOT EXISTS vacancies (
+            id SERIAL PRIMARY KEY,
+            pub_date DATE,
+            update_ts TIMESTAMP WITH TIME ZONE,
+            source_upload_dt DATE,
+            source_type CHARACTER VARYING (20),
+            vacancy CHARACTER VARYING (150),
+            experience CHARACTER VARYING (50),
+            company CHARACTER VARYING (128),
+            link CHARACTER VARYING (100),
+            salary CHARACTER VARYING (50),
+            skills CHARACTER VARYING (300),
+            description VARCHAR
+        );
+        COMMENT ON COLUMN vacancies.pub_date 
+            IS 'Дата публикации вакансии';
+        COMMENT ON COLUMN vacancies.update_ts 
+            IS 'Дата вставки данных в таблицу vacancies';
+        COMMENT ON COLUMN vacancies.source_upload_dt 
+            IS 'Дата создания csv-файла, из которого выгружается информация';
+        COMMENT ON COLUMN vacancies.source_type 
+            IS 'Источник полученной инфомарции о вакансиях ';
+        COMMENT ON COLUMN vacancies.vacancy 
+            IS 'Наименование вакансии';
+        COMMENT ON COLUMN vacancies.experience 
+            IS 'Требуемый опыт';
+        COMMENT ON COLUMN vacancies.company 
+            IS 'Название компании';
+        COMMENT ON COLUMN vacancies.link 
+            IS 'Ссылка на вакансию';
+        COMMENT ON COLUMN vacancies.salary 
+            IS 'Заработная плата';
+        COMMENT ON COLUMN vacancies.skills 
+            IS 'Ключевые навыки, требуемые в вакансии'; 
+        COMMENT ON COLUMN vacancies.description 
+            IS 'Описание вакансии';
+    """
+
+    conn = get_pg_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
