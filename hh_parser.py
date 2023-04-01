efc5d5a2f3043b4e402b4cff5f05efc15fa54299
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-04-01 18:48:26 +0300

    correction of remarks

--- hh_parser.py
+++ hh_parser.py
@@ -10,7 +10,8 @@ from parsel import Selector
 from datetime import datetime
 from typing import List, Tuple
 
-DIR_NAME = 'data'
+OUTPUT_FILEPATH_TEMPLATE = 'data/{source_type}/{specialization}/{source_upload_dt}'
+
 
 def parse_args():
     """Getting  data from the user from the command line for searching"""
@@ -20,6 +21,7 @@ def parse_args():
     args = parser.parse_args()
     return vars(args)
 
+
 def get_links(url: str) -> List[str]:
     """Extract job links from given page
     :param url: link to first page
@@ -28,7 +30,8 @@ def get_links(url: str) -> List[str]:
 
     response = requests.get(url, headers=headers)
     job_content = response.content.decode('utf-8')
-    links_sel = Selector(job_content).xpath('/html').xpath("//div[contains(@class, 'HH-MainContent HH-Supernova-MainContent')]")
+    links_sel = Selector(job_content).xpath('/html').xpath(
+        "//div[contains(@class, 'HH-MainContent HH-Supernova-MainContent')]")
     for l_sell in links_sel:
         links_page = l_sell.xpath(
             "//a[contains(@class,'serp-item__title')]/@href").getall()
@@ -57,7 +60,7 @@ def get_vacancy_data(link: str) -> Tuple[str]:
     # find the date and convert to the desired format
     try:
         date = html_sel.xpath("//p[contains(@class, 'vacancy-creation-time-redesigned')]"
-                                   "//text()").getall()[1]
+                              "//text()").getall()[1]
         date = re.sub(r'[^\d,^\w.]', ' ', date)
         locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
         pub_date = datetime.strptime(date, "%d %B %Y")
@@ -85,24 +88,20 @@ def get_vacancy_data(link: str) -> Tuple[str]:
     return pub_date, vacancy_name, experience, company, link, salary, skill, text_vacancy
 
 
-def save_data(data_vacancies: List[Tuple]):
-    """We form the received information into the scv file. The file is saved in the folder "result"
-    :param data_vacancies: list with information on vacancies
+def save_data(data: List[Tuple], output_filepath: str):
+    """We form the received information into the scv file."
+    :param data: list with information on vacancies
+    :param output_filepath: path to save the file
     """
 
-    source_type = 'hh'
-    specialization = args["name"].replace("-", "_")
-    source_upload_dt = str(datetime.now().date())
-    path_dir = str(Path(DIR_NAME, source_type, specialization, source_upload_dt))
+    if not os.path.exists(output_filepath):
+        os.makedirs(output_filepath)
 
-    if not os.path.exists(path_dir):
-        os.makedirs(path_dir)
+    file = str(Path(output_filepath, 'vacancies.csv'))
 
-    name_file = str(Path(path_dir, 'vacancies.csv'))
-
-    with open(name_file, 'wt') as f:
+    with open(file, 'wt') as f:
         csv_out = csv.writer(f)
-        csv_out.writerows(data_vacancies)
+        csv_out.writerows(data)
 
 
 if __name__ == '__main__':
@@ -145,4 +144,9 @@ if __name__ == '__main__':
         time.sleep(2)
 
     # csv file generation
-    save_data(data_vacancies)
+    save_data(
+        data=data_vacancies,
+        output_filepath=OUTPUT_FILEPATH_TEMPLATE.format(source_type='hh',
+                                                        specialization=args["name"].replace('-', '_'),
+                                                        source_upload_dt=datetime.now().date())
+    )
