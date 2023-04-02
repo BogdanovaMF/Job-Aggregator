89f17d33ab8fc848480afcdc2e13f14c35888f89
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-04-02 06:41:17 +0300

    logging script to file and console

--- hh_parser.py
+++ hh_parser.py
@@ -1,8 +1,10 @@
 import re
 import os
 import csv
+import sys
 import time
 import locale
+import logging
 import argparse
 import requests
 from pathlib import Path
@@ -12,12 +14,25 @@ from typing import List, Tuple
 
 OUTPUT_FILEPATH_TEMPLATE = 'data/{source_type}/{specialization}/{source_upload_dt}'
 
+def get_logger():
+    """Object logging function"""
+
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter("%(levelname)s  %(asctime)s: %(message)s"))
+    logger.addHandler(handler)
+
+    fh = logging.FileHandler('file.log')
+    fh.setFormatter(logging.Formatter("%(levelname)s  %(asctime)s: %(message)s"))
+    logger.addHandler(fh)
+    return logger
 
 def parse_args():
     """Getting  data from the user from the command line for searching"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', required=True, choices=['data-engineer'])
+    parser.add_argument('--specialization', required=True, choices=['data-engineer'])
     args = parser.parse_args()
     return vars(args)
 
@@ -66,12 +81,14 @@ def get_vacancy_data(link: str) -> Tuple[str]:
         pub_date = datetime.strptime(date, "%d %B %Y")
 
     except:
+        logger.exception(f'Date processing error, {link}')
         try:
             date = html_sel.xpath("//p[contains(@class, 'vacancy-creation-time')]//text()").getall()[1]
             date = re.sub(r'[^\d,^\w.]', ' ', date)
             locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
             pub_date = datetime.strptime(date, "%d %B %Y")
         except:
+            logger.exception(f'Date processing error, {link}')
             pub_date = None
 
     skill = html_sel.xpath("//div[contains(@class, 'bloko-tag-list')]//text()").getall()
@@ -84,7 +101,7 @@ def get_vacancy_data(link: str) -> Tuple[str]:
     salary = ' '.join(salary)
 
     experience = html_sel.xpath("//span[contains(@data-qa, 'vacancy-experience')]//text()").get()
-    print(f'Получаем инфомарцию по вакансии {vacancy_name}, компания "{company.strip()}"')
+    logger.info(f'Receive information about the vacancy "{vacancy_name}", company "{company.strip()}"')
     return pub_date, vacancy_name, experience, company, link, salary, skill, text_vacancy
 
 
@@ -107,11 +124,13 @@ def save_data(data: List[Tuple], output_filepath: str):
 if __name__ == '__main__':
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
                              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+    logger = get_logger()
 
     args = parse_args()
+
     url = 'https://hh.ru/vacancies/{specialization}?customDomain=1&{num_page}'
 
-    job_hh = requests.get(url.format(specialization=args["name"], num_page='page=0'), headers=headers)
+    job_hh = requests.get(url.format(specialization=args["specialization"], num_page='page=0'), headers=headers)
 
     init_content = job_hh.content.decode('utf-8')
     html_sel = Selector(init_content).xpath('//html')[0]
@@ -119,7 +138,7 @@ if __name__ == '__main__':
     # find the number of available vacancies
     search_quantity = html_sel.xpath("//div[contains(@class, 'main-content')]")
     quantity = search_quantity.xpath("//span[contains(@data-qa, 'vacancies-total-found')]//text()").get()
-    print(f'Всего {quantity[2:]}')
+    logger.info(f'Всего {quantity[2:]}')
 
     page_nav_buttons = html_sel.xpath(
         '//div[contains(@class, "pager")]/span[contains(@class, "pager-item-not-in-short-range")]'
@@ -131,13 +150,16 @@ if __name__ == '__main__':
     # list links for all vacancies from all pages
     links = []
     for i in range(total_pages):
-        links += get_links(url.format(specialization=args["name"], num_page=f'page={i}'))
+        links += get_links(url.format(specialization=args["specialization"], num_page=f'page={i}'))
+        logger.info(f'Page №{i + 1}/{total_pages}: {args["specialization"].replace("-", " ")}')
         time.sleep(0.7)
+    logger.info(f'{len(links)} links found')
 
     # job listing
     data_vacancies = []
     try:
-        for link in links:
+        for i, link in enumerate(links):
+            logger.info(f'Processed vacancy №{i + 1}')
             data_vacancies.append(get_vacancy_data(link))
             time.sleep(0.7)
     except ConnectionError:
@@ -147,6 +169,6 @@ if __name__ == '__main__':
     save_data(
         data=data_vacancies,
         output_filepath=OUTPUT_FILEPATH_TEMPLATE.format(source_type='hh',
-                                                        specialization=args["name"].replace('-', '_'),
+                                                        specialization=args["specialization"].replace('-', '_'),
                                                         source_upload_dt=datetime.now().date())
     )
