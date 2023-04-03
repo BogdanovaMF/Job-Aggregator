184e5dd8b957faa22c71c7d7bd7ba4cf8c2d5f13
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-04-03 19:49:35 +0300

    fixing bugs

--- etl/clients/hh.py
+++ etl/clients/hh.py
@@ -1,19 +1,21 @@
+import sys
+sys.path.append('..')
+
 import re
-import os
-import csv
 import time
 import locale
 import requests
-from pathlib import Path
+from datetime import date
 from parsel import Selector
-from typing import List, Tuple
 from datetime import datetime
-
-from .utils import logger
-
+from typing import List, Tuple
+from etl.utils import get_logger, save_data
+from etl.config import OUTPUT_FILEPATH_TEMPLATE
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+
+logger = get_logger()
 
 
 class HHClient:
@@ -22,10 +24,11 @@ class HHClient:
     url = 'https://hh.ru/vacancies/{specialization}?customDomain=1&{num_page}'
     source_type = 'hh'
 
-    def parse_and_save(self, specialization):
+    @classmethod
+    def parse_and_save(cls, specialization):
         """Parsing start method"""
 
-        job_hh = requests.get(self.url.format(specialization=specialization, num_page='page=0'), headers=headers)
+        job_hh = requests.get(cls.url.format(specialization=specialization, num_page='page=0'), headers=headers)
 
         init_content = job_hh.content.decode('utf-8')
         html_sel = Selector(init_content).xpath('//html')[0]
@@ -45,23 +48,30 @@ class HHClient:
         # list links for all vacancies from all pages
         links = []
         for i in range(total_pages):
-            links += self._get_links(self.url.format(specialization=specialization, num_page=f'page={i}'))
+            links += cls._get_links(cls.url.format(specialization=specialization, num_page=f'page={i}'))
             logger.info(f'Page №{i + 1}/{total_pages}: {specialization.replace("-", " ")}')
             time.sleep(0.7)
         logger.info(f'{len(links)} links found')
 
         # job listing
-        data_vacancies = []
+        data = []
         try:
             for i, link in enumerate(links):
                 logger.info(f'Processed vacancy №{i + 1}')
-                data_vacancies.append(self._get_vacancy_data(link))
+                data.append(cls._get_vacancy_data(link))
                 time.sleep(0.7)
 
         except ConnectionError:
             time.sleep(2)
 
-        return data_vacancies
+        save_data(
+            data=data,
+            output_filepath=OUTPUT_FILEPATH_TEMPLATE.format(
+                source_type=cls.source_type,
+                specialization=specialization.replace('-', '_'),
+                source_upload_dt=date.today()
+            )
+        )
 
     @staticmethod
     def _get_links(url: str) -> List[str]:
@@ -71,17 +81,18 @@ class HHClient:
         """
 
         response = requests.get(url, headers=headers)
-
         job_content = response.content.decode('utf-8')
-        links_sel = Selector(job_content).xpath('/html').xpath(
-            "//div[contains(@class, 'HH-MainContent HH-Supernova-MainContent')]")
-        for l_sell in links_sel:
-            links_page = l_sell.xpath(
-                "//a[contains(@class,'serp-item__title')]/@href").getall()
-        return links_page
 
-    @classmethod
-    def _get_vacancy_data(cls, link: str) -> Tuple[str]:
+        links_sel = Selector(job_content).xpath("//div[contains(@class, 'HH-MainContent HH-Supernova-MainContent')]")
+        if links_sel:
+            for l_sell in links_sel:
+                links_page = l_sell.xpath("//a[contains(@class,'serp-item__title')]/@href").getall()
+                return links_page
+        else:
+            raise IndexError(f'Failed to parse links to vacancies by url={url}')
+
+    @staticmethod
+    def _get_vacancy_data(link: str) -> Tuple[str]:
         """Method parses vacancy data from given url hmtl
         :param link: job posting link
         :return: job data tuple
@@ -90,15 +101,20 @@ class HHClient:
         response = requests.get(link, headers=headers)
         html_sel = Selector(response.content.decode('utf-8')).xpath('/html')[0]
 
-        info_v = html_sel.xpath("//div[contains(@class, 'vacancy-section')]").xpath(
-            "//div[contains(@data-qa, 'vacancy-description')]//text()").getall()
-        text_vacancy = ''.join(info_v)  # merged into a string, removed extra characters
+        text_vacancy = html_sel.xpath("//div[contains(@data-qa, 'vacancy-description')]//text()").getall()
+        text_vacancy = ' '.join([i.strip() for i in text_vacancy if i.strip()])
 
         vacancy_name = str(html_sel.xpath("//h1[contains(@data-qa,'vacancy-title')]/text()").get())
-        company = html_sel.xpath("//a[contains(@data-qa,'vacancy-company-name')]//text()").getall()
-        company = ' '.join(company)
-        company = re.sub(r'[^\d,^\w.]', ' ', company)
-        company = company[0:(len(company) // 2 + 1)]
+
+        company = html_sel.xpath("//a[contains(@data-qa,'vacancy-company-name')]//text()").get()
+
+        skill = html_sel.xpath("//div[contains(@class, 'bloko-tag-list')]//text()").getall()
+        skill = ','.join([i for i in skill])
+
+        salary = html_sel.xpath("//div[contains(@data-qa, 'vacancy-salary')]//text()").getall()
+        salary = ' '.join([i for i in ' '.join(salary).strip().split()])
+
+        experience = html_sel.xpath("//span[contains(@data-qa, 'vacancy-experience')]//text()").get()
 
         # find the date and convert to the desired format
         try:
@@ -119,33 +135,5 @@ class HHClient:
                 logger.exception(f'Date processing error, {link}')
                 pub_date = None
 
-        skill = html_sel.xpath("//div[contains(@class, 'bloko-tag-list')]//text()").getall()
-        skill = ','.join(skill)
-        skill = re.sub(r'[^\w.]', ' ', skill)
-
-        salary = html_sel.xpath(
-            "//div[contains(@data-qa, 'vacancy-salary')]//text()").getall()
-        salary = ' '.join(salary).split()
-        salary = ' '.join(salary)
-
-        experience = html_sel.xpath("//span[contains(@data-qa, 'vacancy-experience')]//text()").get()
         logger.info(f'Receive information about the vacancy "{vacancy_name}", company "{company.strip()}"')
         return pub_date, vacancy_name, experience, company, link, salary, skill, text_vacancy
-
-    def _save_data(self, data: List[Tuple], output_filepath: str):
-        """We form the received information into the scv file."
-        :param data: list with information on vacancies
-        :param output_filepath: path to save the file
-        """
-
-        if not os.path.exists(output_filepath):
-            os.makedirs(output_filepath)
-
-        file = str(Path(output_filepath, 'vacancies.csv'))
-
-        with open(file, 'wt') as f:
-            csv_out = csv.writer(f)
-            csv_out.writerows(data)
-
-
-
--- etl/main.py
+++ etl/main.py
@@ -1,8 +1,5 @@
 import argparse
-from datetime import date
 from clients.hh import HHClient
-from clients.config import OUTPUT_FILEPATH_TEMPLATE
-
 
 def parse_args():
     """Getting  data from the user from the command line for searching"""
@@ -16,9 +13,4 @@ def parse_args():
 if __name__ == '__main__':
 
     args = parse_args()
-
-    hh = HHClient()
-    data = hh.parse_and_save(args['specialization'])
-    hh._save_data(data=data, output_filepath=OUTPUT_FILEPATH_TEMPLATE.format(source_type='hh',
-                                                                             specialization=args['specialization'],
-                                                                             source_upload_dt=date.today()))
+    HHClient.parse_and_save(args["specialization"])
