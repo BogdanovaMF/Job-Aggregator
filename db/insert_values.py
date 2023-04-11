493e137728ff8ccf96236c73e618f714091f946c
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-04-11 14:16:29 +0300

    fix errors when writing to pg

--- db/insert_values.py
+++ db/insert_values.py
@@ -9,105 +9,50 @@ from db.utilities import get_pg_connection
 class PostgresLoader:
     """Class for writing data to PostgreSQL"""
 
-    def __init__(self, conn, cursor):
+    def __init__(self, conn):
         self.conn = conn
-        self.cursor = cursor
+        self.cursor = self.conn.cursor()
 
-    def _create_temp_table(self, target_table: str, data: List[Tuple], source_type: str, specialization: str):
+    def create_temp_table(self, target_table: str, col_names: List, data: List[Tuple]):
         """Creating and insert data into a temporary table
         :param target_table: target table name
+        :param col_names: list of table columns
         :param data: data to write to the table
-        :param source_type: the type of resource from which the data came
-        :param specialization: specialization for which vacancies are searched
         :return: temporary table name
         """
 
         current_tt = int(time.time())
-        query = f"""
-            CREATE TEMP TABLE {target_table}_{current_tt} (
-                pub_date DATE,
-                update_ts TIMESTAMP WITH TIME ZONE,
-                source_upload_dt DATE,
-                source_type VARCHAR,
-                vacancy VARCHAR,
-                experience VARCHAR,
-                company VARCHAR,
-                link VARCHAR,
-                salary VARCHAR,
-                skills VARCHAR,
-                description VARCHAR,
-                specialization VARCHAR
-            );
-            """
-        self.cursor.execute(query)
-
-        guery_insert_temp_table = f"""
-            INSERT INTO {target_table}_{current_tt}(
-                pub_date,
-                update_ts,
-                source_upload_dt,
-                source_type,
-                vacancy,
-                experience,
-                company,
-                link,
-                salary,
-                skills,
-                description,
-                specialization
-                )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        query_create = f"""
+            CREATE TEMP TABLE {target_table}_{current_tt} 
+            AS SELECT {', '.join(col_names)} 
+            FROM {target_table} LIMIT 0
             ;
-            """
+        """
+        self.cursor.execute(query_create)
 
-        # file creation date
-        source_upload_dt = datetime.now().date()
-
-        # date the table was modified
-        update_ts = datetime.now()
-
-        for i in data:
-            line = i[0], update_ts, source_upload_dt, source_type, i[1], i[2], i[3], i[4], i[5], i[6], i[
-                7], specialization
-            self.cursor.execute(guery_insert_temp_table, line)
+        item_placeholders = ', '.join(['%s'] * len(col_names))
+        guery_insert_temp_table = f"""
+            INSERT INTO {target_table}_{current_tt} 
+            ({', '.join(col_names)})
+            VALUES ({item_placeholders})
+            ;
+        """
+        self.cursor.executemany(guery_insert_temp_table, data)
+        self.conn.commit()
 
         return f'{target_table}_{current_tt}'
 
-    def insert_and_update(self, target_table: str):
+    def insert_and_update(self, target_table: str, temp_table: str, col_names: List):
         """Update and insert value into target data table from temporary table
         :param target_table: target table name
+        :param temp_table: temporary table name
+        :param col_names: list of table columns
         """
 
-        temp_table = self._create_temp_table(target_table, data, source_type, specialization)
-
         guery_insert_target_table = f"""
-            INSERT INTO {target_table}(
-                pub_date,
-                update_ts,
-                source_upload_dt,
-                source_type,
-                vacancy,
-                experience,
-                company,
-                link,
-                salary,
-                skills,
-                description,
-                specialization
-                )
-            SELECT
-                pub_date,
-                update_ts,
-                source_upload_dt,
-                source_type,
-                vacancy,
-                experience,
-                company,
-                link,
-                salary,
-                skills,
-                description,
-                specialization
+            INSERT INTO {target_table}
+            ({', '.join(col_names)})
+            SELECT {', '.join(col_names)}
             FROM {temp_table}
             ON CONFLICT
             DO NOTHING
@@ -116,83 +61,80 @@ class PostgresLoader:
         self.cursor.execute(guery_insert_target_table)
         self.conn.commit()
 
-    def insert_from_temp_into_target(self, target_table: str, delete_condition):
+    def insert_from_temp_into_target(self, target_table: str, temp_table: str, col_names: List, delete_condition=None):
         """Deleting data and adding new data
         :param target_table: target table name
+        :param temp_table: temporary table name
+        :param col_names: list of table columns
         :param delete_condition: condition for deleting data
         """
 
-        temp_table = self._create_temp_table(target_table, data, source_type, specialization)
         q1 = f"""
-            DELETE FROM {target_table} 
-            WHERE specialization ='{delete_condition[0]}'
-              AND source_type='{delete_condition[1]}' 
-              AND source_upload_dt='{delete_condition[2]}'
-            ;
+            DELETE FROM {target_table}
+            {delete_condition}
+            ;   
         """
         q2 = f"""
-            INSERT INTO {target_table}(
-                pub_date,
-                update_ts,
-                source_upload_dt,
-                source_type,
-                vacancy,
-                experience,
-                company,
-                link,
-                salary,
-                skills,
-                description,
-                specialization
-                )
-            SELECT
-                pub_date,
-                update_ts,
-                source_upload_dt,
-                source_type,
-                vacancy,
-                experience,
-                company,
-                link,
-                salary,
-                skills,
-                description,
-                specialization
+            INSERT INTO {target_table} (
+                {', '.join(col_names)}
+            )
+            SELECT {', '.join(col_names)}
             FROM {temp_table}
             ;
         """
-        self.cursor.execute(q1)
-        self.cursor.execute(q2)
-        self.conn.commit()
+
+        try:
+            if delete_condition is not None:
+                self.cursor.execute(q1)
+                self.cursor.execute(q2)
+                self.conn.commit()
+            else:
+                self.cursor.execute(q2)
+                self.conn.commit()
+        except:
+            self.conn.rollback()
+            self.conn.commit()
+        finally:
+            self.conn.close()
 
 
 if __name__ == '__main__':
 
     target_table = 'vacancies'
-
-    # file creation date
     source_upload_dt = datetime.now().date()
-
-    # Source of information received about vacancies
     source_type = 'hh'
-
+    update_ts = datetime.now()
     specialization = 'data-engineer'
 
-    delete_condition = (specialization, source_type, source_upload_dt)
+    delete_condition = f"""
+        WHERE  specialization='{specialization}'
+        AND source_type='{source_type}' 
+        AND source_upload_dt='{source_upload_dt}'
+    """
 
-    file = 'etl/data/{source_type}/{specialization}/{source_upload_dt}/vacancies.csv'.format(
+    OUTPUT_FILEPATH_TEMPLATE = 'data/{source_type}/{specialization}/{source_upload_dt}'.format(
         source_type=source_type,
         specialization=specialization,
         source_upload_dt=source_upload_dt)
-
+    file = f'etl/{OUTPUT_FILEPATH_TEMPLATE}/vacancies.csv'
     pathfile = str(Path(Path.cwd(), file))
 
     with open(pathfile) as f:
         file_reader = csv.reader(f, delimiter=",")
         data = list(file_reader)
+        data_from_csv = []  # creating list of data tuples from csv
+        for row in data:
+            line = row[0], update_ts, source_upload_dt, source_type, row[1], row[2], row[3], row[4], row[5], row[6], \
+                row[7], specialization
+            data_from_csv.append(line)
 
     conn = get_pg_connection()
-    cursor = conn.cursor()
-    pg_loader = PostgresLoader(conn=conn, cursor=cursor)
+    curr = conn.cursor()
 
-    pg_loader.insert_from_temp_into_target(target_table, delete_condition)
+    curr.execute(f"SELECT * FROM {target_table} LIMIT 0")  # select columns name from target_table
+    col_names = [col.name for col in curr.description][1:]  # identified columns without id
+
+    pg_loader = PostgresLoader(conn=conn)
+    temp_table = pg_loader.create_temp_table(target_table, col_names, data_from_csv)
+
+    pg_loader.insert_from_temp_into_target(target_table, temp_table, col_names, delete_condition)
