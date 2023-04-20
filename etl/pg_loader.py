1700fff3a0c00aff84e961048e4a54f4a544fc66
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-04-20 12:40:34 +0300

    fix errors

--- etl/pg_loader.py
+++ etl/pg_loader.py
@@ -2,7 +2,7 @@ import csv
 import time
 import argparse
 from datetime import datetime
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 
 from utils import get_logger
 from utils import get_pg_connection
@@ -22,7 +22,7 @@ def parse_args():
     return vars(args)
 
 
-class PostgresLoader():
+class PostgresLoader:
     """Class for writing data to PostgreSQL"""
 
     def __init__(self, conn, logger) -> None:
@@ -30,19 +30,36 @@ class PostgresLoader():
         self.cursor = self.conn.cursor()
         self.logger = logger if logger else get_logger()
 
-    def create_temp_table(self, target_table: str, data: List[Tuple]):
-        """Creating and insert data into a temporary table
+    def insert_data_into_table(self, table_name: str, data: List[Tuple]) -> None:
+        """Creating and insert data into a table
+        :param table_name: tables name for insert values
+        :param data: data to write to the table
+        """
+
+        self.cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")  # select columns name from table
+        col_names = [col.name for col in self.cursor.description if col.name != 'id']  # identified columns without id
+        try:
+            guery_insert_data = f"""
+                INSERT INTO {table_name} 
+                    ({', '.join(col_names)})
+                VALUES ({', '.join(['%s'] * len(col_names))});
+            """
+            self.cursor.executemany(guery_insert_data, data)
+            self.conn.commit()
+            self.logger.info(f'Inserting values into a table {table_name} completed successfully')
+        except:
+            self.logger.error(f'Error. No data entered into table {table_name}')
+            self.conn.rollback()
+
+    def insert_from_temp_into_target(self, target_table: str, data: List[Tuple], delete_condition: Optional[str] = None) -> None:
+        """Deleting data and adding new data
         :param target_table: target table name
         :param data: data to write to the table
-        :return: temporary table name
+        :param delete_condition: condition for deleting data
         """
 
         temp_table = f'{target_table}_{int(time.time())}'
-        self.cursor.execute(f"SELECT * FROM {target_table} LIMIT 0")  # select columns name from target_table
-        col_names = [col.name for col in self.cursor.description if col.name != 'id']  # identified columns without id
-
         query_create = f"""CREATE TEMP TABLE {temp_table} AS TABLE {target_table} WITH NO DATA"""
-        self.logger.info(f'Create temporary table {temp_table}')
         try:
             self.cursor.execute(query_create)
             self.conn.commit()
@@ -52,75 +69,30 @@ class PostgresLoader():
             self.conn.rollback()
 
         self.logger.info(f'Inserting values into a temporary table {temp_table}')
-        try:
-            guery_insert_temp_table = f"""
-                INSERT INTO {temp_table} 
-                    ({', '.join(col_names)})
-                VALUES ({', '.join(['%s'] * len(col_names))});
-            """
-            self.cursor.executemany(guery_insert_temp_table, data)
-            self.conn.commit()
-            self.logger.info(f'Inserting values into a temporary table {temp_table} completed successfully')
-        except:
-            self.logger.error('Error. No data entered into temporary table')
-            self.conn.rollback()
+        self.insert_data_into_table(temp_table, data)
 
-        return temp_table, col_names
+        self.cursor.execute(f"SELECT * FROM {target_table} LIMIT 0")  # select columns name from target_table
+        col_names = [col.name for col in self.cursor.description if col.name != 'id']  # identified columns without id
 
-    def insert_and_update(self, target_table: str, data: List[Tuple]) -> None:
-        """Update and insert value into target data table from temporary table
-        :param target_table: target table name
-        :param data: data to write to the table
-        """
-
-        temp_table, col_names = self.create_temp_table(target_table, data)
-        self.logger.info(f'Update and insert value into target table {target_table} from temporary table {temp_table}')
-        guery_insert_target_table = f"""
-            INSERT INTO {target_table}
-                ({', '.join(col_names)})
-            SELECT {', '.join(col_names)}
-            FROM {temp_table}
-            ON CONFLICT
-            DO NOTHING;
-        """
-        try:
-            self.cursor.execute(guery_insert_target_table)
-            self.conn.commit()
-            self.logger.info(
-                f'Update and insert table {target_table} data from temporary table {temp_table} completed successfully')
-        except:
-            self.logger.info('Error update and insert')
-            self.conn.rollback()
-
-    def insert_from_temp_into_target(self, target_table: str, data: List[Tuple], delete_condition=None) -> None:
-        """Deleting data and adding new data
-        :param target_table: target table name
-        :param data: data to write to the table
-        :param delete_condition: condition for deleting data
-        """
-        temp_table, col_names = self.create_temp_table(target_table, data)
         queries = []
         if delete_condition:
             q_delete = f"""DELETE FROM {target_table} {delete_condition};"""
             queries.append(q_delete)
-
-        q_insert = f"""
-            INSERT INTO {target_table} (
-                {', '.join(col_names)}
-            )
-            SELECT {', '.join(col_names)}
-            FROM {temp_table};
-        """
+        q_insert = f"""INSERT INTO {target_table} ({', '.join(col_names)}) SELECT {', '.join(col_names)} FROM {temp_table};"""
         queries.append(q_insert)
 
         try:
             for query in queries:
                 self.cursor.execute(query)
             self.conn.commit()
-            self.logger.info('Data entered in the target table')
+            self.logger.info(f'Inserting data from a temporary table {temp_table} into the targer table "{target_table}"')
         except:
             self.logger.error('Query Execution Error')
             self.conn.rollback()
+
+        self.cursor.execute(f"DROP TABLE {temp_table};")
+        self.conn.commit()
+        self.logger.info(f'Table {temp_table} deleted')
 
     def __del__(self):
         self.logger.info('Closing the connection')
@@ -128,7 +100,6 @@ class PostgresLoader():
 
 
 if __name__ == '__main__':
-
     args = parse_args()
 
     filepath = OUTPUT_FILEPATH_TEMPLATE.format(
@@ -139,14 +110,9 @@ if __name__ == '__main__':
 
     update_ts = datetime.now()
     with open(output_filepath) as f:
-        file_reader = csv.reader(f, delimiter=",")
-        data = list(file_reader)
-        data_from_csv = []  # creating list of data tuples from csv
-        for row in data:
-            line = row[0], update_ts, args['source_upload_dt'], args['source_type'], row[1], row[2], row[3], row[4], \
-                row[5], row[6], \
-                row[7], args['specialization']
-            data_from_csv.append(line)
+        data = list(csv.reader(f, delimiter=","))
+        data_from_csv = [(row[0], update_ts, args['source_upload_dt'], args['source_type'], row[1], row[2], row[3], row[4],
+             row[5], row[6], row[7], args['specialization']) for row in data]  # creating list of data tuples from csv
 
     delete_condition = f"""
         WHERE  specialization='{args['specialization']}'
